b228b5d4cce9c8972354a834928b2f50dd9e2425
rafasoftwares <rcarvalho_r@hotmail.com>
2024-10-16 20:04:30 -0300

    Criando a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
